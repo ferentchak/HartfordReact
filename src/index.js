e7ac85f6babab4ffc6eb6594e0c5d4757bf3b2fb
Charles Ferentchak <charles@customagile.com>
2020-12-07 09:39:19 -0700

    toast charts

--- src/index.js
+++ src/index.js
@@ -1,25 +1,63 @@
 import React from "react";
 import { render } from "react-dom";
-import Highcharts from "highcharts/highstock";
-import HighchartsReact from "highcharts-react-official";
+import 'tui-chart/dist/tui-chart.css'
+import { ColumnChart, LineChart } from '@toast-ui/react-chart'
 
-const options = {
-  title: {
-    text: "My stock chart"
-  },
+var data = {
+  categories: ['June, 2015', 'July, 2015', 'August, 2015', 'September, 2015', 'October, 2015', 'November, 2015', 'December, 2015'],
   series: [
     {
-      data: [1, 2, 1, 4, 3, 6, 7, 3, 8, 6, 9]
-    }
+      name: 'Budget',
+      data: [5000, 3000, 5000, 7000, 6000, 4000, 1000]
+    },
+    
   ]
 };
+var options = {
+  chart: {
+    width: 1160,
+    height: 650,
+    title: 'Monthly Revenue',
+    format: '1,000'
+  },
+  yAxis: {
+    title: 'Amount',
+    min: 0,
+    max: 9000
+  },
+  xAxis: {
+    title: 'Month'
+  },
+  legend: {
+    align: 'top'
+  }
+};
+
+
+class MyComponent extends React.Component {
+  chartRef = React.createRef();
+  debugger;
+  handleClickButton = () => {
+    console.log('type:', this.chartRef.current.getInstance().getType());
+  }
+
+  render() {
+    return (
+      <>
+        <ColumnChart
+          ref={this.chartRef}
+          data={data}
+          options={options}
+        />
+        <button onClick={this.handleClickButton}>getType</button>
+      </>
+    );
+  }
+}
 
 const App = () => (
   <div>
-    <HighchartsReact
-      highcharts={Highcharts}
-      constructorType={"stockChart"}
-      options={options}
+    <MyComponent
     />
   </div>
 );
